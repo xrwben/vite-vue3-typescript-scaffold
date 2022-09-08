73888d659e7e04f4927895ada3590949c8027c4a
liben <759761629@qq.com>
2022-09-09 00:56:50 +0800

    feat：配置eslint、暂时禁用prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,42 +3,54 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
   extends: [
     'eslint:recommended',
-    // 'plugin:vue/vue3-recommended',
-    'plugin:vue/vue3-strongly-recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:vue/vue3-recommended',
+    // 'plugin:vue/vue3-strongly-recommended',
+    'plugin:@typescript-eslint/recommended',
+    // "prettier",
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      globalReturn: false,
+      impliedStrict: false,
+      jsx: true,
+    },
   },
+  // plugins：eslint-plugin-vue @typescript-eslint/eslint-plugin eslint-plugin-prettier的缩写
   plugins: [
     'vue',
-    '@typescript-eslint'
+    '@typescript-eslint',
+    // "prettier"
   ],
   rules: {
-    'indet': ['error', 2],
-    'quotes': ['error', 'single'],
-    'arrow-spacing': 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['warn', 'never'],
+    // 'arrow-spacing': 'error',
+    // 'space-before-blocks': 'warn',
     // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // eslint-pugin-vue规则
     'vue/multi-word-component-names': 'off',
     'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
     'vue/html-self-closing': ['error', {
-      'html': {
-        'void': 'never',
-        'normal': 'any',
-        'component': 'any'
+      html: {
+        void: 'never',
+        normal: 'any',
+        component: 'any',
       },
-      'svg': 'always',
-      'math': 'always'
+      svg: 'always',
+      math: 'always',
     }],
-    // "@typescript-eslint/no-explicit-any": ["off"]
-  }
+    '@typescript-eslint/no-explicit-any': ['off'],
+    // "prettier/prettier": "warn",
+  },
 }
--- src/views/explain/index.vue
+++ src/views/explain/index.vue
@@ -1,40 +1,53 @@
 <template>
-  <div class="explain">
-    <span>说明页</span>
-  </div>
+  <div class="explain">说明页</div>
   <img src="@/assert/pic.jpg" alt="">
   <!-- <img src="/img.jpg" alt=""> -->
+  <router-view></router-view>
+  <router-link to="/result">1</router-link>
 </template>
 
 <script setup lang="ts">
-  import { Toast } from 'vant';
-  import { RouterView, RouterLink } from 'vue-router';
-  console.log(import.meta.env)
-  Toast.loading({
-    duration: 3000,
-    message: '加载中...',
-    forbidClick: true,
-    loadingType: 'spinner',
-  });
-
-  const abc: number = '123'
-  console.log(abc)
-  if(abc){
-    console.log(1)
+import {} from 'vue'
+import { Toast } from 'vant'
+import { RouterView, RouterLink } from 'vue-router'
+console.log(import.meta.env)
+Toast.loading({
+  duration: 3000,
+  message: '加载中...',
+  forbidClick: true,
+  loadingType: 'spinner'
+})
+var aa
+const abc = '123'
+interface Obj {
+  [x: string]: any;
+}
+const obj: Obj = {
+  name: 'daben',
+  age: 18,
+  like: {
+    a: '1',
+    b: '2'
   }
-  var fileArr = [
-    'http://demo.ntsgw.oa.com/api/sso/content-center/api/v1/content/doc/batch-download?signature=3c087c76ac7d4aabed76ffc07310e052&compress_name=中文sadda通用配置没有开启能力项评估',
-    'http://demo.ntsgw.oa.com/api/sso/content-center/api/v1/content/doc/batch-download?signature=3c087c76ac7d4aabed76ffc07310e052&compress_name=中文sadda通用配置没有开启评委评审记录',
-    'http://demo.ntsgw.oa.com/api/sso/content-center/api/v1/content/doc/batch-download?signature=3c087c76ac7d4aabed76ffc07310e052&compress_name=中文sadda阿斯顿发生'
-  ]
-  fileArr.forEach(item => {
-    console.log(item)
-    // window.open(item)
-  });
+}
+obj.sex = '男'
+console.log(abc)
+if (abc) {
+  console.log(1)
+}
+var fileArr = [
+  'http://demo.ntsgw.oa.com/api/sso/content-center/api/v1/content/doc/batch-download?signature=3c087c76ac7d4aabed76ffc07310e052&compress_name=中文sadda通用配置没有开启能力项评估',
+  'http://demo.ntsgw.oa.com/api/sso/content-center/api/v1/content/doc/batch-download?signature=3c087c76ac7d4aabed76ffc07310e052&compress_name=中文sadda通用配置没有开启评委评审记录',
+  'http://demo.ntsgw.oa.com/api/sso/content-center/api/v1/content/doc/batch-download?signature=3c087c76ac7d4aabed76ffc07310e052&compress_name=中文sadda阿斯顿发生',
+]
+fileArr.forEach((item) => {
+  console.log(item)
+  // window.open(item)
+})
 </script>
 
 <style lang="less" scoped>
-  .explain {
-    color: red;
-  }
+.explain {
+  color: red;
+}
 </style>
